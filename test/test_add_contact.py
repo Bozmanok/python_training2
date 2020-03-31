cd1700f21a1212a2d63ffd802a5bd7b39e792f7a
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2020-03-31 13:05:20 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="first name", middlename="middle name", lastname="lasr name",
                                     nickname="nickname", path_to_photo="C:\\Users\\Asus\\Desktop\\test_image.jfif",
                                     title="title", company="company",
@@ -21,13 +21,13 @@ def test_add_contact(app):
                                     email3="e-mail3", homepage="homepage", bday="5", bmonth="February",
                                     byear="1990", aday="14", amonth="June", ayear="2000", new_group="[none]",
                                     address2="secondary address", phone2="secondary home", notes="secondary notes"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", path_to_photo="",
                                     title="", company="", address="", home="", mobile="", work="", fax="", email="",
                                     email2="", email3="", homepage="", bday="", bmonth="-", byear="", aday="",
                                     amonth="-", ayear="", new_group="", address2="", phone2="", notes=""))
-    app.logout()
+    app.session.logout()
