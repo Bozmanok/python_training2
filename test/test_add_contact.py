9fa433e309b11429e51e47bf741266d0833e2352
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2020-04-02 22:26:44 +0300

    Реализована проверка валидации фикстуры перед выполнением теста. Добавлена проверка текущего состояния сессии.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="first name", middlename="middle name", lastname="lasr name",
                                nickname="nickname", path_to_photo="C:\\Users\\Asus\\Desktop\\test_image.jfif",
                                title="title", company="company",
@@ -12,13 +11,10 @@ def test_add_contact(app):
                                email3="e-mail3", homepage="homepage", bday="5", bmonth="February",
                                byear="1990", aday="14", amonth="June", ayear="2000", new_group="[none]",
                                address2="secondary address", phone2="secondary home", notes="secondary notes"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                title="", company="", address="", home="", mobile="", work="", fax="", email="",
                                email2="", email3="", homepage="", bday="", bmonth="-", byear="", aday="",
                                amonth="-", ayear="", new_group="", address2="", phone2="", notes=""))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,11 +2,8 @@ from model.contact import Contact
 
 
 def test_change_first_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
-    app.session.logout()
 
 
 def test_change_first_contact_nickname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(nickname="New nickname"))
