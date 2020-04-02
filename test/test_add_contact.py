c00c9720bda029afa602534d8ac8bcfab619aa7a
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2020-04-02 21:31:47 +0300

    Исправлены наименования некоторых тестов и методов, установлены значения по умолчанию для конструкторов группы и контакта, декомпозированы методы по заполнению форм

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -17,7 +17,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", path_to_photo="",
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                title="", company="", address="", home="", mobile="", work="", fax="", email="",
                                email2="", email3="", homepage="", bday="", bmonth="-", byear="", aday="",
                                amonth="-", ayear="", new_group="", address2="", phone2="", notes=""))
--- test/test_change_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_change_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.change_first_group(Group(name="test1 edit", header="test2 edit", footer="test3 edit"))
-    app.session.logout()
